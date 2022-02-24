970fa3edbfae2614e13370039ea4c010a0b17e47
Markus Teufel <markus@freeletics.com>
2022-02-24 19:24:04 +0000

    removes unused code

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 
 	"github.com/reinerspass/waldego/internal"
 
@@ -15,63 +13,6 @@ import (
 	_ "github.com/heroku/x/hmetrics/onload"
 	_ "github.com/lib/pq"
 )
-
-func deckMock(c *gin.Context) {
-	c.String(http.StatusOK, `{
-		"decks": [
-			{
-				"id": 1,
-				"name": "Des hört sich spanisch ah"
-			},
-			{
-				"id": 2,
-				"name": "Des hört sich französich ah"
-			},
-			{
-				"id": 3,
-				"name": "Des hört sich dütsch ah"
-			},
-			{
-				"id": 4,
-				"name": "Des hört sich englisch ah"
-			}
-		]
-	}`)
-}
-
-func dbFunc(db *sql.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error creating database table: %q", err))
-			return
-		}
-
-		if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error incrementing tick: %q", err))
-			return
-		}
-
-		rows, err := db.Query("SELECT tick FROM ticks")
-		if err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error reading ticks: %q", err))
-			return
-		}
-
-		defer rows.Close()
-		for rows.Next() {
-			var tick time.Time
-			if err := rows.Scan(&tick); err != nil {
-				c.String(http.StatusInternalServerError,
-					fmt.Sprintf("Error scanning ticks: %q", err))
-				return
-			}
-			c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
-		}
-	}
-}
 
 func main() {
 	port := os.Getenv("PORT")
